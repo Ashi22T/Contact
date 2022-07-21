8b052e2208ca75f5cc1eac613900ac9f44e762c1
Ashani <93838102+Ashi22T@users.noreply.github.com>
2022-07-21 19:10:48 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,100 @@
+ <p align="center">
+      <img src="https://image.flaticon.com/icons/png/512/88/88271.png" width="90px" alt="Image FlatIcon"/>
+</p>
+
+<h1 align="center">Contacts</h1>
+
+   <h2 align="center">Topics 📋</h2>
+
+   <p>
+   
+   - [About 📖](#about-)
+   - [Preview 📱](#preview-)
+   - [Challenges and Learnings along the way 🤯](#challenges-and-learnings-along-the-way-)
+   - [How to Use 🤔](#how-to-use-)
+   - [How to Contribute 💪](#how-to-contribute-)
+   - [License 📝](#license-)
+
+   </p>
+
+<h2 align="center">About 📖</h2>
+   
+<p align="center">   
+   A Contact Book made with Flutter. 💙<br>
+   A Contact Application, using <a href="https://pub.dev/packages/sqflite">sqflite</a>, a packpage from Flutter. Where you can create, consult, edit and delete, also organize them from A-Z, or from Z-A, put photos (from gallery) and email in your contacts.<br>
+   This application is in version 1.2.
+</p>
+
+---
+
+<h2 align="center">Preview 📱</h2><br>
+
+   <p align="center">
+      <img src="assets/demos/contactsdemo1.gif">
+   </p>
+   
+   > * See all in [`demo`](https://github.com/felipecastrosales/Contacts/tree/master/assets/demos) folder. 🧐📂
+
+---
+
+<h2 align="center">Challenges and Learnings along the way 🤯</h2>
+
+   <p>
+      In this application, which is a little more complex, I learned and also reinforced many previous lessons.<br>
+      I used new widgets, resources and this example of CRUD (Create, Read, Update and Delete), made me learn a lot, it will certainly be a learning experience that I will take with me in all the technologies that I will learn. <br>
+      In this project I also used the SQLite database, with the Flutter plugin, the <a href="https://flutter.dev/docs/cookbook/persistence/sqlite"> sqflite</a>, which will help insert, query, update and delete queries.
+   </p>
+
+---
+
+<h2 align="center">How to Use 🤔</h2>
+
+   ```   
+   - Clone this repository:
+   $ git clone https://github.com/felipecastrosales/Contacts contacts
+
+   - Enter in directory:
+   $ cd contacts
+
+   - For install dependencies:
+   $ flutter packages get
+
+   - Run the app: 
+   $ flutter run
+   ```
+
+---
+
+<h2 align="center">How to Contribute 💪</h2>
+
+   ```
+   - Fork the project 
+
+   - Create a new branch with your changes:
+   $ git checkout -b my-feature
+
+   - Save your changes and create a commit message telling you what you did:
+   $ git commit -m "feature: My new feature"
+
+   - Submit your changes:
+   $ git push origin my-feature
+   ```
+
+---
+
+<h2 align="center">License 📝</h2>
+
+   This repository is under MIT license. You can see the [LICENSE](https://github.com/felipecastrosales/Contacts/blob/master/LICENSE) file for more details. 😉
+
+   ---
+
+   >This project was developed with ❤️ by **[@Felipe Sales](https://www.linkedin.com/in/felipecastrosales/)**, with the instructor **[Daniel Ciolfi](https://linkedin.com/in/danielciolfi)**, in the course  **["Creating Android and iOS Apps with Flutter - Create 16 Apps"](https://www.udemy.com/course/curso-completo-flutter-app-android-ios/?referralCode=1355952A966260D40D18)** on **[Udemy](https://www.udemy.com/)**.<br>
+   If it helped you, give ⭐, contribute, it will help me too 😉
+
+---
+
+   <div align="center">
+
+   [![Linkedin Badge](https://img.shields.io/badge/-Felipe%20Sales-292929?style=flat-square&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/felipecastrosales/)](https://www.linkedin.com/in/felipecastrosales/)
+
+   </div>
---
+++ lib/ui/contact_page.dart
@@ -0,0 +1,154 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import '../helpers/contact_helper.dart';
+
+class ContactPage extends StatefulWidget {
+  final Contact contact;
+  ContactPage({this.contact});
+
+  @override
+  _ContactPageState createState() => _ContactPageState();
+}
+
+class _ContactPageState extends State<ContactPage> {
+  final _nameController  = TextEditingController();
+  final _emailController = TextEditingController();
+  final _phoneController = TextEditingController();
+  final _nameFocus = FocusNode();
+  bool _userEdited = false;
+  Contact _editedContact;
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.contact == null) {
+      _editedContact = Contact();
+    } else {
+      _editedContact = Contact.fromMap(widget.contact.toMap());
+    }
+
+    _nameController.text  = _editedContact.name;
+    _emailController.text = _editedContact.email;
+    _phoneController.text = _editedContact.phone;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: _requestPop,
+      child: Scaffold(
+        appBar: AppBar(
+            title: Text(_editedContact.name ?? 'Add'),
+            backgroundColor: Colors.purpleAccent,
+            centerTitle: true
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            if (_editedContact.name != null && _editedContact.name.isNotEmpty) {
+              Navigator.pop(context, _editedContact);
+            } else {
+              FocusScope.of(context).requestFocus(_nameFocus);
+            }
+          },
+          child: Icon(Icons.add),
+          backgroundColor: Colors.purpleAccent,
+        ),
+        body: SingleChildScrollView(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            children: <Widget>[
+              GestureDetector(
+                child: Container(
+                  width: 150,
+                  height: 150,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: _editedContact.img != null
+                          ? FileImage(File(_editedContact.img))
+                          : AssetImage('assets/images/person.png'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                onTap: () async {
+                  await ImagePicker()
+                      .getImage(source: ImageSource.gallery)
+                      .then((file) {
+                    if (file == null) return;
+                    setState(() {
+                      _editedContact.img = file.path;
+                    });
+                  });
+                },
+              ),
+              TextField(
+                controller: _nameController,
+                focusNode: _nameFocus,
+                decoration: InputDecoration(labelText: 'Name'),
+                onChanged: (text) {
+                  _userEdited = true;
+                  setState(() {
+                    _editedContact.name = text;
+                  });
+                },
+              ),
+              TextField(
+                controller: _emailController,
+                decoration: InputDecoration(labelText: 'Email'),
+                onChanged: (text) {
+                  _userEdited = true;
+                  _editedContact.email = text;
+                },
+                keyboardType: TextInputType.emailAddress,
+              ),
+              TextField(
+                controller: _phoneController,
+                decoration: InputDecoration(labelText: 'Phone Number'),
+                onChanged: (text) {
+                  _userEdited = true;
+                  _editedContact.phone = text;
+                },
+                keyboardType: TextInputType.phone,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<bool> _requestPop() {
+    if (_userEdited) {
+      showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: Text('Discard changes?'),
+              content: Text('If you exit, your changes will be lost.'),
+              actions: <Widget>[
+                TextButton(
+                  child: Text('Cancel'),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                TextButton(
+                  child: Text('Sim'),
+                  onPressed: () {
+                    Navigator.pop(context);
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            );
+          }
+      );
+      return Future.value(false);
+    } else {
+      return Future.value(true);
+    }
+  }
+}
